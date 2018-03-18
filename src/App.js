80fea2746ba93dcd966999a6e6f8f6bd6318ae00
RadekM86 <byrm.design@gmail.com>
2018-03-18 13:37:20 +0100

    Added css module

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css'
 
 class App extends Component {
   render() {
+
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Workflow App</h1>
+      <div className={classes.App}>
+        <header className={classes.AppHeader}>
+          <img src={logo} className={classes.AppLogo} alt="logo" />
+          <h1 className={classes.AppTitle}>Workflow App</h1>
         </header>
+        <p className={classes.AppIntro}>
+        Paragraph
+        </p>
       </div>
     );
   }
